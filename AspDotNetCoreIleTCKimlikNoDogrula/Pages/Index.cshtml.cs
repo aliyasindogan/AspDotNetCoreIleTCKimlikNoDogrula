93bed5c8c6ad808aa86c31971337ba2a6a532a5b
aliyasindogan <a.yasindogan@gmail.com>
2021-11-22 02:52:43 +0300

    Add project files.

---
+++ AspDotNetCoreIleTCKimlikNoDogrula/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MernisServis;
+
+namespace AspDotNetCoreIleTCKimlikNoDogrula.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async void OnGet()
+        {
+            var client = new MernisServis.KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            var response = await client.TCKimlikNoDogrulaAsync(Convert.ToInt64(30461282111), "ALİ YASİN", "DOĞAN", 1979);
+            var result = response.Body.TCKimlikNoDogrulaResult;
+        }
+    }
+}
